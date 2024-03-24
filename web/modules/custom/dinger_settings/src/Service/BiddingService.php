78f4a7f6bcd22883e2c0741fe66403f265d71523
medard <medard.rebero@gmail.com>
2024-03-24 18:38:36 +0100

    Control bid updates

--- web/modules/custom/dinger_settings/src/Service/BiddingService.php
+++ web/modules/custom/dinger_settings/src/Service/BiddingService.php
@@ -4,6 +4,8 @@ namespace Drupal\dinger_settings\Service;
 
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Entity\EntityBase;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
@@ -62,13 +64,54 @@ class BiddingService {
     $call = $bid->get('field_bid_call')->entity;
     $call->get('field_call_bids')->appendItem(['target_id' => $bid->id()]);
 
-    $bidStatus = $bid->get('field_bid_status')->getString();
-    if ($bidStatus === 'confirmed') {
+    $bidType = $bid->get('field_bid_type')->getString();
+    if ($bidType === 'accept') {
       $this->processConfirmedBid($bid);
     }
   }
 
+  public function findCallConfirmedBid(NodeInterface $call): NodeInterface|null {
+    if ($call->bundle() !== 'call') {
+      throw new BadRequestHttpException('Node should be a call, but is ' . $call->bundle());
+    }
+
+    try {
+      $confirmedBidId = $this->entityTypeManager
+        ->getStorage('node')
+        ->getQuery()->accessCheck(FALSE)
+        ->condition('type', 'bid')
+        ->condition('field_bid_call.target_id', $call->id())
+        ->condition('field_bid_status', 'confirmed')
+        ->execute();
+
+      if (count($confirmedBidId) > 1) {
+        $this->logger->error(t('Invalid State. Call $callId has more than 1 confirmed bid.', ['$callId' => $call->id()]));
+        return null;
+      }
+
+      if (count($confirmedBidId) == 0) {
+        return null;
+      }
+
+      /** @var $bid NodeInterface */
+      $bid = Node::load(reset($confirmedBidId));
+      return $bid;
+    }
+    catch (InvalidPluginDefinitionException|PluginNotFoundException $e) {
+      $this->logger->error($e);
+      return null;
+    }
+  }
+
   private function processConfirmedBid(NodeInterface $bid): void {
+    $bidStatus = $bid->get('field_bid_status')->getString();
+    if ($bidStatus !== 'confirmed') {
+      throw new BadRequestHttpException(t('Bid @id has invalid status. @invalid should be confirmed', [
+        '@id' => $bid->id(),
+        '@status' => $bidStatus
+      ]));
+    }
+
     try {
       /** @var $call NodeInterface */
       $call = $bid->get('field_bid_call')->entity;
