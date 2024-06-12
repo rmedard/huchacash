62e78d21cd4dfb53b9706dd71ab2a1f6bff58ae9
medard <medard.rebero@gmail.com>
2024-06-12 13:11:03 +0200

    activate grpc and protobuf

--- web/modules/custom/dinger_settings/src/Service/FirestoreCloudService.php
+++ web/modules/custom/dinger_settings/src/Service/FirestoreCloudService.php
@@ -1,13 +1,29 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace Drupal\dinger_settings\Service;
 
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\Core\Site\Settings;
 use Google\Cloud\Firestore\FirestoreClient;
 use Google\Cloud\Firestore\Transaction;
 
-class FirestoreCloudService {
+final class FirestoreCloudService {
 
+  /**
+   * Drupal\Core\Logger\LoggerChannelFactory definition.
+   *
+   * @var LoggerChannelInterface
+   */
+  protected LoggerChannelInterface $logger;
+
+  /**
+   * @param LoggerChannelFactory $logger
+   */
+  public function __construct(LoggerChannelFactory $logger)
+  {
+    $this->logger = $logger->get('firestore_service');
+  }
   /**
    * @param $callId
    * @param $orderNumber
@@ -15,11 +31,10 @@ class FirestoreCloudService {
    * @throws \Google\Cloud\Core\Exception\GoogleException
    */
   public function setCallOrderNumber($callId, $orderNumber): void {
+    $this->logger->info('Firestore Cloud: Setting order number. CallId: @callId | OrderNbr: @orderNbr', ['@callId' => $callId, '@orderNbr' => $orderNumber]);
     $settingsFileLocation = Settings::get('gc_tasks_settings_file');
-//    $credentialsData = file_get_contents($gcSettingsFileLocation);
-//    $credentialsArray = json_decode($credentialsData, true);
     $firestoreClient = new FirestoreClient(['keyFilePath' => $settingsFileLocation]);
-    $callReference = $firestoreClient->collection('calls')->document($callId);
+    $callReference = $firestoreClient->collection('live_calls')->document($callId);
     $firestoreClient->runTransaction(function(Transaction $transaction) use ($callReference, $orderNumber) {
       $transaction->update($callReference, [
         [
