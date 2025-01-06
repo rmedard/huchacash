eb32bec100b877094dfc4a1f5fc317655368249d
medard <medard.rebero@gmail.com>
2025-01-06 06:11:53 +0100

    Fix onOrderPresave

---
+++ web/modules/custom/dinger_settings/src/Plugin/Action/BaseHuchaGcAction.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\dinger_settings\Plugin\Action;
+
+use Drupal\Core\Access\AccessResultAllowed;
+use Drupal\Core\Access\AccessResultForbidden;
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\dinger_settings\Service\GoogleCloudService;
+use Drupal\dinger_settings\Utils\GcNodeType;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+abstract class BaseHuchaGcAction extends ActionBase implements ContainerFactoryPluginInterface {
+
+  const string GC_TASK_FIELD_NAME = 'field_gc_task_name';
+
+  /**
+   * @var LoggerChannelInterface
+   */
+  protected LoggerChannelInterface $logger;
+
+  /**
+   * @var GoogleCloudService
+   */
+  protected GoogleCloudService $googleCloudService;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactoryInterface $loggerFactory, GoogleCloudService $googleCloudService) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->logger = $loggerFactory->get(static::class);
+    $this->googleCloudService = $googleCloudService;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory'),
+      $container->get('dinger_settings.google_cloud_service')
+    );
+  }
+
+  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE): bool|AccessResultInterface {
+    $isAllowed = $object instanceof NodeInterface && in_array($object->bundle(), ['order', 'call']);
+    return $isAllowed ? new AccessResultAllowed() : new AccessResultForbidden();
+  }
+
+  protected function getTriggerTime(NodeInterface $node): DrupalDateTime {
+    return match ($node->bundle()) {
+      GcNodeType::CALL => $node->get('field_call_expiry_time')->date,
+      GcNodeType::ORDER => $node->get('field_order_delivery_time')->date,
+      default => throw new BadRequestHttpException('Unsupported Node Type'),
+    };
+  }
+
+  abstract public function execute(NodeInterface $entity = NULL): void;
+}
--- web/modules/custom/dinger_settings/src/Plugin/Action/CreateHuchaGcAction.php
+++ web/modules/custom/dinger_settings/src/Plugin/Action/CreateHuchaGcAction.php
@@ -2,25 +2,11 @@
 
 namespace Drupal\dinger_settings\Plugin\Action;
 
-use Drupal\Core\Access\AccessResultAllowed;
-use Drupal\Core\Access\AccessResultForbidden;
-use Drupal\Core\Access\AccessResultInterface;
-use Drupal\Core\Action\ActionBase;
 use Drupal\Core\Action\Attribute\Action;
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\Core\Entity\EntityStorageException;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Logger\LoggerChannelInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\dinger_settings\Service\GoogleCloudService;
-use Drupal\dinger_settings\Utils\GcNodeType;
 use Drupal\node\NodeInterface;
 use Google\ApiCore\ApiException;
 use Google\ApiCore\ValidationException;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 #[Action(
   id: 'create_gc_task_action',
@@ -28,33 +14,7 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
   category: new TranslatableMarkup('Custom'),
   type: 'node'
 )]
-final class CreateHuchaGcAction extends ActionBase implements ContainerFactoryPluginInterface {
-
-  const string GC_TASK_FIELD_NAME = 'field_gc_task_name';
-  /**
-   * @var LoggerChannelInterface
-   */
-  protected LoggerChannelInterface $logger;
-
-  /**
-   * @var GoogleCloudService
-   */
-  public GoogleCloudService $googleCloudService;
-
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactoryInterface $loggerFactory, GoogleCloudService $googleCloudService) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->logger = $loggerFactory->get('CreateHuchaGcAction');
-    $this->googleCloudService = $googleCloudService;
-  }
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): CreateHuchaGcAction {
-    return new CreateHuchaGcAction(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('logger.factory'),
-      $container->get('dinger_settings.google_cloud_service'),
-    );
-  }
+final class CreateHuchaGcAction extends BaseHuchaGcAction {
 
   public function execute(NodeInterface $entity = NULL): void {
     /**
@@ -71,19 +31,5 @@ final class CreateHuchaGcAction extends ActionBase implements ContainerFactoryPl
     catch (ApiException|ValidationException $e) {
       $this->logger->error($e);
     }
-  }
-
-  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE): bool|AccessResultInterface
-  {
-    $isAllowed = $object instanceof NodeInterface && in_array($object->bundle(), ['order', 'call']);
-    return $isAllowed ? new AccessResultAllowed() : new AccessResultForbidden();
-  }
-
-  private function getTriggerTime(NodeInterface $node): DrupalDateTime {
-    return match ($node->bundle()) {
-      GcNodeType::CALL => $node->get('field_call_expiry_time')->date,
-      GcNodeType::ORDER => $node->get('field_order_delivery_time')->date,
-      default => throw new BadRequestHttpException('Unsupported Node Type'),
-    };
   }
 }
