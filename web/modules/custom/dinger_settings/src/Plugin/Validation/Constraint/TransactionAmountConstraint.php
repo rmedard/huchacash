f2adcc53e2cb3832c53c64a4958bd725c33fe775
medard <medard.rebero@gmail.com>
2025-01-31 17:36:33 +0100

    Upgrade composer libraries

--- web/modules/custom/dinger_settings/src/Plugin/Validation/Constraint/TransactionAmountConstraint.php
+++ web/modules/custom/dinger_settings/src/Plugin/Validation/Constraint/TransactionAmountConstraint.php
@@ -4,22 +4,14 @@ namespace Drupal\dinger_settings\Plugin\Validation\Constraint;
 
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Validation\Attribute\Constraint;
-use Symfony\Component\Validator\Constraint as SymfonyConstraint;
 
 #[Constraint(
   id: 'TransactionAmountConstraint',
   label: new TranslatableMarkup('Transaction Amount Constraint'),
   type: 'entity'
 )]
-class TransactionAmountConstraint extends SymfonyConstraint {
+class TransactionAmountConstraint extends TransactionConstraintBase {
 
   public string $message = '%value is greater than available balance for this customer.';
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validatedBy(): string
-  {
-    return TransactionConstraintsValidator::class;
-  }
 }
