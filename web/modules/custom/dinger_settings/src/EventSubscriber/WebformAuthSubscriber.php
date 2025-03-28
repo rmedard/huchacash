c8aafbe49e5721042c7198ddc1018fa967b9a099
medard <medard.rebero@gmail.com>
2025-03-28 15:11:30 +0100

    Authenticate webform requests

---
+++ web/modules/custom/dinger_settings/src/EventSubscriber/WebformAuthSubscriber.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dinger_settings\EventSubscriber;
+
+use Drupal;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\simple_oauth\Authentication\Provider\SimpleOauthAuthenticationProvider;
+use Exception;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Subscriber meant to catch webform requests and attempt to authenticate them
+ */
+final readonly class WebformAuthSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Drupal\Core\Logger\LoggerChannelFactory definition.
+   *
+   * @var LoggerChannelInterface
+   */
+  protected LoggerChannelInterface $logger;
+
+  /**
+   * Constructs a WebformAuthSubscriber object.
+   */
+  public function __construct(
+    private SimpleOauthAuthenticationProvider $authenticationProvider, LoggerChannelFactory $logger
+  ) {
+    $this->logger = $logger->get('WebformAuthSubscriber');
+  }
+
+  public function authenticateRequest(RequestEvent $event): void
+  {
+    $request = $event->getRequest();
+
+    $paths = ['/webform/bug_reporting'];
+
+    // Check if this is a webform submission route
+    if (in_array($request->getPathInfo(), $paths)) {
+      if ($request->headers->has('Authorization')) {
+        try {
+          $account = $this->authenticationProvider->authenticate($request);
+          if ($account) {
+            Drupal::currentUser()->setAccount($account);
+          }
+        }
+        catch (Exception $e) {
+          $this->logger->error('OAuth authentication failed: @error', ['@error' => $e->getMessage()]);
+          $response = new JsonResponse(
+            ['error' => 'Invalid OAuth token'],
+            401
+          );
+          $event->setResponse($response);
+        }
+      } else {
+        $this->logger->info('Unauthenticated OAuth webform request');
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      KernelEvents::REQUEST => ['authenticateRequest', 300]
+    ];
+  }
+
+}
