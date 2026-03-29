7bc65892d71493cd1be60a505de0d1ccd4107d62
medard <medard.rebero@gmail.com>
2026-03-29 01:58:18 +0100

    Fix Me Resource endpoint

--- web/modules/custom/dinger_settings/src/Controller/MeApiController.php
+++ web/modules/custom/dinger_settings/src/Controller/MeApiController.php
@@ -2,35 +2,59 @@
 
 namespace Drupal\dinger_settings\Controller;
 
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
-class MeApiController extends ControllerBase {
+final class MeApiController extends ControllerBase {
 
-  public function get() {
+  protected EntityStorageInterface $nodeStorage;
+
+  /**
+   * @param EntityStorageInterface $nodeStorage
+   */
+  public function __construct(EntityStorageInterface $nodeStorage)
+  {
+    $this->nodeStorage = $nodeStorage;
+  }
+
+  /**
+   * @throws InvalidPluginDefinitionException
+   * @throws PluginNotFoundException
+   */
+  public static function create(ContainerInterface $container): self
+  {
+    return new MeApiController($container->get('entity_type.manager')->getStorage('node'));
+  }
+
+
+  public function get(): JsonResponse
+  {
     $user = $this->currentUser();
-
-    // Get the customer node linked to this user
-    $customerIds = \Drupal::entityTypeManager()
-      ->getStorage('node')
+    $customerIds = $this->nodeStorage
       ->getQuery()
       ->condition('type', 'customer')
+      ->condition('status', NodeInterface::PUBLISHED)
       ->condition('field_customer_user.target_id', $user->id())
       ->accessCheck(FALSE)
       ->execute();
 
     if (empty($customerIds)) {
-      return new JsonResponse(['error' => 'Customer profile not found'], 404);
+      return new JsonResponse(['error' => 'Customer profile not found'], Response::HTTP_NOT_FOUND);
     }
 
-    $customer = \Drupal\node\Entity\Node::load(reset($customerIds));
-
-    // Return whatever data structure you need
+    $customer = Node::load(reset($customerIds));
     return new JsonResponse([
       'customer_id' => $customer->uuid(),
-      'reference' => $customer->getTitle(), // or whatever field you need
+      'reference' => $customer->getTitle(),
       'user_id' => $user->id(),
       'user_name' => $user->getAccountName()
-    ], 200);
+    ], Response::HTTP_OK);
   }
 }
